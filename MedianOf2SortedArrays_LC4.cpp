72ee973774cd8cc5df6cd701f1ce2f8c2e3817e6
Akif Aydogmus <maaydogmus26@gmail.com>
2021-11-24 00:32:00 +0300

    Update MedianOf2SortedArrays_LC4.cpp
    format

--- MedianOf2SortedArrays_LC4.cpp
+++ MedianOf2SortedArrays_LC4.cpp
@@ -10,28 +10,27 @@ public:
         int m=nums1.size();
         int n=nums2.size();
         
-	    	// Take minimum sized, input1 should be smaller
-	    	if(m>n)  return findMedianSortedArrays(nums2,nums1);
+	// Take minimum sized, input1 should be smaller
+	if(m>n)  return findMedianSortedArrays(nums2,nums1);
 		
         int low=0,high=m, indexSum=(m+n+1)/2;
         
         while(low<=high){
-		    	  // cut first array at i, second array at indexSum- i
+	   // cut first array at i, second array at indexSum- i
             int partX = (low+high)/2;
             int partY = indexSum-partX;
       
-			      // if part=0, nothing is on left side. Use -INF for maxLeft
+	    // if part=0, nothing is on left side. Use -INF for maxLeft
             // if part=input size, nothing is on right side. Use +INF for minRight
             int mxlftX = (partX==0)? INT_MIN:nums1[partX-1]; // -1 means left 
             int mnRghtX= (partX==m)? INT_MAX:nums1[partX];
             int mxlftY = (partY==0)? INT_MIN:nums2[partY-1];
             int mnRghtY= (partY==n)? INT_MAX:nums2[partY];
 			
-			     // CROSS-CHECK should be applied
+	    // CROSS-CHECK should be applied
             if( mxlftX<=mnRghtY && mxlftY<=mnRghtX ){
                 if((m+n)%2==0) //Even, avg( max of lefts, min of rights )
-                    return (double)(max(mxlftX,mxlftY)+
-									                  min(mnRghtX,mnRghtY))/2;
+                    return (double)( max(mxlftX,mxlftY) + min(mnRghtX,mnRghtY) )/2;
                 else  // Odd items, max of left ones 
                     return (double)(max(mxlftX,mxlftY));
 					
@@ -40,7 +39,6 @@ public:
             else // X value is small, move right
                 low=partX+1;
         }
-      
         return -1.0;
     }
   
